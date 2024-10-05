293a497a1f873539ec0dc032a74039ead9a082aa
Lucky6400 <luckyjain7247@gmail.com>
2024-10-06 01:28:35 +0530

    Revert "change001"
    This reverts commit 2410302bc96b790b87152620c11f6ad0a8759dfa.

--- new_branch_code.ts
+++ new_branch_code.ts
@@ -3,7 +3,3 @@ console.log(`I wrote this file ${new Date().getFullYear() - 2024} years ago.`)
 console.log(`Coming from dev-test-lucky`);
 
 console.log(`this change is for advanced topic`);
-
-console.log('a change 001')
-console.log('a change 002');
-console.log(`for revert`)
